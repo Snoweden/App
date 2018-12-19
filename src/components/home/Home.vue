8318631535d39144c7550b08808a9adbbb2664a9
Masmics <cherlocker3113@gmail.com>
2018-12-18 16:49:37 -0800

    created dynamic weather table

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
     <h1>Hello World</h1>
+    <resortWeather/>
   </div>
 </template>
 
 <script>
+import resortWeather from '../resorts/ResortWeather';
 export default {
-
+  components: {
+    resortWeather
+  }
 };
 </script>
 
--- src/components/resorts/ResortWeather.vue
+++ src/components/resorts/ResortWeather.vue
@@ -1,13 +1,74 @@
 <template>
   <div>
-
+    <tr>
+      <th>
+        Condition
+      </th>
+      <th>
+        Value
+      </th>
+    </tr>
+    <tr>
+      <td>
+        Temperature
+      </td>
+      <td>
+        {{ weather.main.temp }}
+      </td>
+    </tr>
+    <tr>
+      <td>
+        Humidity
+      </td>
+      <td>
+        {{ weather.main.humidity }}
+      </td>
+    </tr>
+    <tr>
+      <td>
+        Wind Speed
+      </td>
+      <td>
+        {{ weather.wind.speed }}
+      </td>
+    </tr>
+      <tr>
+      <td>
+        Cloud Cover
+      </td>
+      <td>
+        {{ weather.clouds.all }}
+      </td>
+    </tr>
+      <tr>
+      <td>
+        General Weather
+      </td>
+      <td>
+        {{ weather.weather[0].description }}
+      </td>
+    </tr>
   </div>
 </template>
 
 <script>
-import 
+import weatherApi from '../../services/weather-api';
 
 export default {
+  data() {
+    return {
+      weather: null //or {}
+    }
+  },
+  created() {
+    weatherApi.getWeather()
+      .then(w => {
+        this.weather = w;
+      });
+    // console.log(this.weather)
+  }
+
+
 
 };
 </script>
