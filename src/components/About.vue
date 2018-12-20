6c7b8421d9a46b0a5b6b42efa1dbd5ffd0d080aa
Christina T. Herlocker <cherlocker3113@gmail.com>
2018-12-20 12:26:23 -0800

    about us css vue and css initial

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,21 +1,24 @@
 <template>
     <section>
         <h2>About the Developers</h2>
-        <div>
-            Shaba
-            <img src="./assets/" id="shaba" alt="">
-        </div>
-        <div>
-            Connor
-        </div>
-        <div>
-            Lance
-        </div>
-        <div>
-            Chris
-        </div>
-
-        
+            <div class="profiles">
+                <div>
+                    <span>Shaba</span>
+                    <img src="../assets/shaba.png" id="shaba" alt="image of shaba">
+                </div>
+                <div>
+                    <span>Connor</span>
+                    <img src="../assets/connor.png" id="shaba" alt="image of connor">
+                </div>
+                <div>
+                    <span>Lance</span>
+                    <img src="../assets/lance.png" id="shaba" alt="image of lance">
+                </div>
+                <div>
+                    <span>Chris</span>
+                    <img src="../assets/chris.png" id="shaba" alt="image of chris">
+                </div>   
+            </div> 
     </section>
 
 </template>
@@ -27,5 +30,33 @@ export default {
 </script>
 
 <style>
-
+section {
+    width: 100vw;
+    height: 100vh;
+    background-repeat: no-repeat;
+}
+h2 {
+    text-align: center;
+    font-size: 3em;
+    color: white;
+}
+.profiles {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+}
+.profiles div {
+    margin: 10px;
+    padding: 5px;
+}
+img {
+    /* border-radius: 45%; */
+    width: 100%;
+}
+span {
+    text-align: center;
+    text-emphasis: bold;
+    color: white;
+    display: block;
+    width: 100%;
+}
 </style>
