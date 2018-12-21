fe615be660a99a947aae85ae5f15e31b0fa4e90a
Christina T. Herlocker <cherlocker3113@gmail.com>
2018-12-20 20:00:58 -0800

    added conversion from kelvins to fahrenheit

--- src/services/weather-api.js
+++ src/services/weather-api.js
@@ -1,6 +1,6 @@
 export default {
   getWeather(lat = '', lon = '') {
-    return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${encodeURIComponent(lat)}&lon=${encodeURIComponent(lon)}&appid=9abd4dd837ed2f8c11ab1eb3a10377c6`)
+    return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${encodeURIComponent(lat)}&lon=${encodeURIComponent(lon)}&appid=9abd4dd837ed2f8c11ab1eb3a10377c6&units=imperial`)
       .then(response => response.json());
   }
 };
